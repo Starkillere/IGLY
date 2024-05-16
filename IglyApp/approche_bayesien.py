3e1e182877953c0ec47e509637d3d939c1d59bb7
Ayouba Anrezki <ayouba@P1.S-Synthia>
2024-05-16 20:16:19 +0300

    Il pleure dans mon cœur
    Comme il pleut sur la ville ;

---
+++ IglyApp/approche_bayesien.py
@@ -0,0 +1,84 @@
+"""
+    IGLY (Iel gonna let you) 
+    modele : Naif_KNN
+    langage : python3
+    createur : AYOUBA Anrezki
+    initalisation : 14/05/2024 11h14 (en cous d'informatique au lycée clémenceau Nantes 44)
+    MAJ : 14/05/2024
+    Dépots : https://github.com/Starkillere/IGLY
+"""
+
+__all__ =  ["ModeleKNN"]
+
+# Importation
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+
+def chargement_des_donnees() -> tuple:
+
+    """
+    """
+
+        # Importation des données
+
+    df_questions = pd.read_csv('IglyApp/data/q_divorce.csv', delimiter=';', encoding='utf-8')
+    df_reponses = pd.read_csv('IglyApp/data/result_divorce.csv', delimiter=';', encoding='utf-8')
+
+        # Renommer les colonnes du dataframe des réponses avec les entêtes des questions
+
+    df_reponses.columns = df_questions['cle'].tolist() + ['Class']
+
+        # Séparation des fonctionnalités
+
+    X = df_reponses.iloc[:, :-1] # données réponses questions
+
+    y = df_reponses['Class'] # Situation
+
+        # Diviser les données en ensembles de formation et de test
+
+    return (X, y)
+
+class Situation:
+    
+    """
+
+    """
+
+    MARIER =  1
+    DIVORCER = 0
+
+class ModeleBayesien:
+
+    """
+
+    """
+
+    def __init__(self) -> None:
+        self.__g_model = GaussianNB()
+        self.__X, self.__y = chargement_des_donnees()
+        self.__entrainement()
+
+    def __entrainement(self) :
+        self.__g_model.fit(self.__X, self.__y)
+    
+    def prediction(self, nouvel_utilisateur_reponses:list):
+
+        assert type(nouvel_utilisateur_reponses[0]) == list and [l for l in nouvel_utilisateur_reponses [0] if type(l) == int] == nouvel_utilisateur_reponses[0]
+
+        probabilites = self.__g_model.predict_proba(nouvel_utilisateur_reponses)[0]
+        classes =self.__g_model.classes_
+        resultat = {classes[i]: round(probabilites[i],2) for i in range(len(classes))}
+        print(resultat)
+        t_result = {}
+
+        for k in resultat.keys():
+            if k == Situation.MARIER:
+                t_result["DIVORCER"] = resultat[k]*100
+            else:
+                t_result["MARIER"] = resultat[k]*100
+
+        return t_result
--- IglyApp/views.py
+++ IglyApp/views.py
@@ -12,7 +12,7 @@
 
 from flask import Flask, render_template, request, url_for, redirect
 import pandas as pd
-from . import modele_naif_knn
+from . import modele_naif_knn, approche_bayesien
 from datetime import timedelta
 
 app = Flask(__name__)
@@ -20,19 +20,31 @@ app.config.from_object("config")
 app.secret_key = app.config["SECRET_KEY"]
 app.permanent_session_lifetime = timedelta(days=5)
 my_ai = modele_naif_knn.ModeleKNN()
+my_bayes = approche_bayesien.ModeleBayesien()
 df_questions = pd.read_csv('IglyApp//data/q_divorce.csv', delimiter=';', encoding='utf-8')
+position_des_questions_non_compatible = [6, 30, 31, 32, 33, 34, 35, 37, 38, 39, 40, 41, 43, 46, 48, 49, 51, 52]
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+
+    def correcteur(x:int) -> int:
+        l =  [0,1,2,3,4]
+        pos =  l.index(x)
+        return l[-pos-1]
+
+
     if request.method == 'POST':
         reponses_utilisateur = []
         for i in range(1, 55):
             reponse = int(request.form[f'reponse_{i}'])
+            if i+1 in position_des_questions_non_compatible:
+                reponse = correcteur(reponse)
             reponses_utilisateur.append(reponse)
         
         situation_predite = my_ai.prediction(reponses_utilisateur)
+        situation_predite2 = my_bayes.prediction([reponses_utilisateur])
         
-        return render_template('resultat.html', situation_predite=situation_predite)
+        return render_template('resultat.html', situation_predite_f=situation_predite, situation_predite_b=situation_predite2)
     
     return render_template('questionnaire.html', questions=df_questions['Q'].tolist())
